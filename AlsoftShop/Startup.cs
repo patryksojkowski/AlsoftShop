0de77f9cfabe67555548a0221adcef4edfba4eed
Patryk <patryk.sojkowski@hotmail.com>
2022-08-26 18:31:40 +0200

    replaced home controller with simple shop controller

--- AlsoftShop/Startup.cs
+++ AlsoftShop/Startup.cs
@@ -42,7 +42,7 @@ namespace AlsoftShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Shop}/{action=Index}");
             });
         }
     }
